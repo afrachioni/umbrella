931c9c0fb3075ece3197f57616c14b2cbfdf9629
Anthony Frachioni <afrachi1@krakenpf8.nics.utk.edu>
2012-12-17 21:32:22 -0500

    fixed acceptance math

--- ns_driver.cpp
+++ ns_driver.cpp
@@ -43,35 +43,53 @@ int main(int narg, char **arg)
 	// comm_lammps only has 1st P procs (could be all or any subset)
 	MPI_Init(&narg,&arg);
 	parser *p;
+	const int n_windows = 96;
 
 	//MPI things ~~~~~~~~~~~~~~~~~~~~~~~~
 	int me,nprocs;
 	MPI_Comm_rank(MPI_COMM_WORLD,&me);
 	MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
 
-	int nprocs_lammps = nprocs;//TODO parse or ensemble or something
-	if (nprocs_lammps > nprocs) {
-		if (me == 0)
-			printf("ERROR: LAMMPS cannot use more procs than available\n");
-		MPI_Abort(MPI_COMM_WORLD,1);
+	char out_fname[100];
+	sprintf (out_fname, "output/log_%d.txt", me);
+	FILE *local_log = fopen (out_fname, "w");
+	setbuf (local_log, NULL);//debug
+
+	int my_file_index;
+	int file_indices[n_windows];//only the root process needs allocate this
+	float targets[n_windows];
+	if (me == 0)
+	{
+		FILE *in_file = fopen ("sn_window_init.txt", "r");
+		for (int i = 0; i < n_windows; i++) {
+			fscanf (in_file, "%d %f", &file_indices[i], &targets[i]);
+		}
 	}
-	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//TODO scatterv this, assumes n_windows procs, 1 proc/window
+	MPI_Bcast (file_indices, n_windows, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast (targets, n_windows, MPI_FLOAT, 0, MPI_COMM_WORLD);
+	fprintf (stderr, "I am proc %d working on file index %d with target %f.\n", me, file_indices[me], targets[me]);
+	fprintf (local_log, "I am proc %d working on file index %d with target %f.\n", me, file_indices[me], targets[me]);
+
+	MPI_Comm local_comm;
+	//For now, hardcode one proc per window TODO
+	MPI_Comm_split(MPI_COMM_WORLD, me, me, &local_comm);
+
+	int local_rank;
+	MPI_Comm_rank (local_comm, &local_rank);
+
 	p->verbose = me == 0;
 	p = new parser (narg, arg);
 	if (p->parse_error)
 		return 1;
 	
-	int lammps;
-	if (me < nprocs_lammps) lammps = 1;
-	else lammps = MPI_UNDEFINED;
-	MPI_Comm comm_lammps;
-	MPI_Comm_split(MPI_COMM_WORLD,lammps,0,&comm_lammps);
+	int lammps = 1;//TODO clean this up
 	LAMMPS *lmp;
 
 
 
 	if (lammps == 1) {
-		lmp = new LAMMPS(0,NULL,comm_lammps);
+		lmp = new LAMMPS(0,NULL,local_comm);
 		int n;
 		char line[1024];
 		lmp->input->one("#LAMMPS umbrella sampler");
@@ -79,7 +97,7 @@ int main(int narg, char **arg)
 		lmp->input->one("#Hardcoded umbrella script for demo");
 		lmp->input->one("units metal");
 		//sprintf (line, "read_restart %s", p->start);
-		sprintf (line, "read_data %s", p->start);
+		sprintf (line, "read_data quench_init/input/input_sn.%d", file_indices[me]);
 		lmp->input->one(line);
 		lmp->input->one("reset_timestep 0");
 		lmp->input->one("compute myBoop all boop 6 3.4");//TODO parse
@@ -91,7 +109,6 @@ int main(int narg, char **arg)
 
 		//Sn
 		lmp->input->one("pair_style meam");
-		//XXX BSn?
 		lmp->input->one("pair_coeff * * library.meam Sn NULL Sn");//TODO hardcoded
 
 		lmp->input->one("run 0");
@@ -101,57 +118,60 @@ int main(int narg, char **arg)
 		lmp->input->one(line);
 
 		int natoms = lammps_get_natoms (lmp);//XXX uses 32 bit int
-		if (me == 0) {
-			fprintf (stderr, "Initial Q6: %f\n", Q6);
-			fprintf (stderr, "Number of atoms: %d\n", natoms);
+		if (local_rank == 0) {
+			fprintf (local_log, "[%d] Initial Q6: %f\n", me, Q6);
+			fprintf (local_log, "Number of atoms: %d\n", natoms);
 		}
 		double *positions_buffer = new double[3 * natoms];
 		double factor = -0.5 * p->spring / p->temperature;
 		int umbrella_accept = 1;
 		int seed;
 		int accept_count = 0;
-		int64_t last_step_end_time, step_time;
-		int64_t lammps_start_time, lammps_split;
+		int64_t last_step_end_time, step_time, lammps_start_time, lammps_split;
 		last_step_end_time = get_time();
 		double Q6_old = Q6;
-		double d_Q, d_Q_old, bias_potential_new, bias_potential_old;
-		double boltz_factor;
+		double d_Q, d_Q_old, bias_potential_new, bias_potential_old, log_boltz_factor;
+		//Q6_old is last accepted Q6
 		for (int i = 0; i < p->count; i++) {
 			if (!umbrella_accept) {
+				//Umbrella reject
 				//TODO try copying lmp->atom->x to avoid global op.
-				lammps_get_coords (lmp, positions_buffer);
-				//TODO why 1000?
-				if (me == 0) seed = rand() % 1000;
-				MPI_Bcast (&seed, 1, MPI_INT, 0, comm_lammps);
+				lammps_put_coords (lmp, positions_buffer);
+				if (local_rank == 0) seed = rand() % 1000; //TODO why 1000?
+				MPI_Bcast (&seed, 1, MPI_INT, 0, local_comm);
 				sprintf(line, "velocity all create %f %d", p->temperature, seed);
 				lmp->input->one (line);
 			} else {
+				//Umbrella accept
 				accept_count++;
-				lammps_put_coords (lmp, positions_buffer);
+				lammps_get_coords (lmp, positions_buffer);
+				Q6_old = Q6;
+				d_Q_old = Q6_old - targets[me];
+				bias_potential_old = d_Q_old * d_Q_old;
 			}
 			sprintf (line, "run %d", p->duration);
 			lammps_start_time = get_time();
 			lmp->input->one (line);
 			lammps_split = get_time() - lammps_start_time;
 			Q6 = *((double *) lammps_extract_compute(lmp,(char*)"myBoop", 0, 0));
-			if (me == 0) {
-				d_Q_old = Q6_old - p->target_min;
-				d_Q = Q6 - p->target_min;
-				bias_potential_old = d_Q_old * d_Q_old;
+			if (local_rank == 0) {
+				d_Q = Q6 - targets[me];
 				bias_potential_new = d_Q * d_Q;
-				boltz_factor = exp(factor*(bias_potential_old-bias_potential_new));
-				umbrella_accept = (((double) rand()) / RAND_MAX) < boltz_factor;
+				log_boltz_factor = factor*(bias_potential_new-bias_potential_old);
+				umbrella_accept = log((double) rand() / RAND_MAX) < log_boltz_factor;
 			}
-			MPI_Bcast (&umbrella_accept, 1, MPI_INT, 0, comm_lammps);
-			Q6_old = Q6;
+			MPI_Bcast (&umbrella_accept, 1, MPI_INT, 0, local_comm);
 			step_time = get_time() - last_step_end_time;
-			if (me == 0)
-				fprintf (stderr, "Step index: %d\tQ6: %f\tBoltzmann factor: %f\tAccept: %d\tAccept rate: %f\tTime: %5lld\tLAMMPS time: %5lld\tOverhead: %f\n", i, Q6, boltz_factor, umbrella_accept, ((double) accept_count) / (i + 1), step_time, lammps_split, ((double ) (step_time - lammps_split)) / step_time);
+			if (local_rank == 0){
+				fprintf (local_log, "[%d] Step index: %d\tQ6: %f\t (log)Boltzmann factor: %f\tAccept: %d\tAccept rate: %f\tTime: %5lld\tLAMMPS time: %5lld\tOverhead: %f\n", me, i, Q6, log_boltz_factor, umbrella_accept, ((double) accept_count) / (i + 1), step_time, lammps_split, ((double ) (step_time - lammps_split)) / step_time);
+				//fprintf (local_log, "\td_Q: %f\td_Q_old: %f\tbias: %f\tbias_old: %f\tfactor: %f\n", d_Q, d_Q_old, bias_potential_new, bias_potential_old, factor);
+			}
 			last_step_end_time = get_time();
 		}
 		delete [] positions_buffer;
 		delete lmp;
 	}
+	fclose (local_log);
 	MPI_Finalize();
 }
 int64_t get_time()
