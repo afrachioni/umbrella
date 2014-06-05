2c1b8e81fd5da1d78063aeef7552fbe5792e1bba
Anthony Frachioni <afrachi1@binghamton.edu>
2014-06-05 15:58:15 -0700

    cleanup in barostat_step

--- barostat_step.cpp
+++ barostat_step.cpp
@@ -13,7 +13,9 @@ double BarostatStep::Vold = 0;
 int BarostatStep::accepted_count = 0;
 int BarostatStep::count = 0;
 
-BarostatStep::BarostatStep(LAMMPS_NS::LAMMPS *lmp, float probability, char* name, Global *global, double pressure) : UmbrellaStep::UmbrellaStep ( lmp, probability, name, global) {
+BarostatStep::BarostatStep(LAMMPS_NS::LAMMPS *lmp, float probability, \
+		char* name, Global *global, double pressure) :\
+		UmbrellaStep::UmbrellaStep ( lmp, probability, name, global) {
 	this->P = pressure;
 	UmbrellaStep::UmbrellaStep(lmp, probability, name, global);
 	is_barostat = 1;
@@ -27,9 +29,9 @@ void BarostatStep::execute_init() {  // XXX sets Uold, Vold once per instance
 }
 
 void BarostatStep::execute_step() {
-	T = *((double *) lammps_extract_compute (lmp,(char*)"thermo_temp", 0, 0));
+	T = *((double *) lammps_extract_compute (lmp, (char*)"thermo_temp", 0, 0));
 	Uold = *((double *) lammps_extract_compute (lmp,(char*)"thermo_pe", 0, 0));
-	Vold = *((double *)lammps_extract_variable(lmp,(char*)"lu_vol",(char*)"all"));
+	Vold=*((double*)lammps_extract_variable(lmp,(char*)"lu_vol",(char*)"all"));
 
 	execute_block (lmp, take_step_block, global);
 
@@ -42,28 +44,12 @@ void BarostatStep::execute_step() {
 	int accept = log (accept_rand) < exp;
 	MPI_Bcast (&accept, 1, MPI_INT, 0, global->local_comm);
 
-	char msg[100];
-	sprintf( msg, "(V - Vold)/eV: %f\tU - Uold: %f\tNkT log (V/Vold): %f",
-			(V - Vold)/eV, U - Uold, N*kb*T*log(V/Vold));
-	//global->debug (msg);
-
-	//sprintf(msg, "N: %d\tk: %f\tT: %f\tlog(V/Vold): %f\n",
-			//N, kb, T, log(V/Vold));
-	sprintf (msg, "V: %f\tVold: %f", V, Vold);
-	//global->debug(msg);
-	
-
-
 	if (accept) {
 		if (logger != NULL) logger->comment ("accept box change");
-		//global->debug("\t\t\t\t\t\taccept");
-		//Uold = U;
-		//Vold = V;
 		execute_accept();
 		++accepted_count;
 	} else {
 		if (logger != NULL) logger->comment ("reject box change");
-		//global->debug("\t\t\t\t\t\t\treject");
 		execute_reject();
 	}
 	++count;
