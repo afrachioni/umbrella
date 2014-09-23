3a2fbc57226d2ca6a710054b830dcfdffd5959c7
Anthony Frachioni <afrachi1@binghamton.edu>
2014-09-23 15:52:55 -0700

    no more rescaling in histogram

--- histogram.cpp
+++ histogram.cpp
@@ -65,8 +65,7 @@ void Histogram::write (FILE *f) {
 	// Write index|count|center
 	for (int i = 0; i < nbins; ++i)
 		if (hist[i])
-			fprintf (f, "%d\t%d\t%f\n", i, hist[i], (min + (i + 0.5) * bin_width) / 3.4); //XXX dirty
-			//fprintf (f, "%d\t%d\t%f\n", i, hist[i], min + (i + 0.5) * bin_width);
+			fprintf (f, "%d\t%d\t%f\n", i, hist[i], min + (i + 0.5) * bin_width);
 	fclose (f);
 }
 
