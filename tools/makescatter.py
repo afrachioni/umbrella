2652b79cb4559448dc222141ad7ebac8cf77869d
Anthony Frachioni <afrachi1@binghamton.edu>
2014-12-18 12:47:45 -0800

    scatter for all logs

--- tools/makescatter.py
+++ tools/makescatter.py
@@ -7,21 +7,28 @@ import matplotlib.pyplot as plt
 import pylab
 import re
 import numpy
+import os
 
 min = 0
 max = -1
 
-i = 0
-for i in range(40):
-	print "Working on log of index " + str(i)
-	fname = "logs/log_" + str(i) + ".txt"
-	data_file = open (fname, "r")
-	for line in data_file:
-		if re.match ("#Step", line):
-			break
-	data_file.close()
-	headers = line.split()
-	data = numpy.loadtxt(fname)
+dirname = "log_sub/" # must end in path separator
+dirname = "logs/" # must end in path separator
+filenames = os.listdir(dirname)
+filenames.sort(key=lambda x:int(re.search("\d+", x).group(0)))
+
+
+for filename in filenames:
+	if re.search(".lammps", filename) or re.search(".hist", filename):
+		continue
+	print "Working on log: " + filename
+	#data_file = open (dirname + filename, "r")
+	#for line in data_file:
+	#	if re.match ("#Step", line):
+	#		break
+	#data_file.close()
+	#headers = line.split()
+	data = numpy.loadtxt(dirname + filename)
 
 	for col in [1]: #range(len(data[0])):
 		plt.plot(data[min:max,col])
@@ -30,4 +37,6 @@ for i in range(40):
 		#pylab.savefig("plots/" + headers[col] + "_" + str(i) + ".png")
 		#plt.clf()
 
+plt.xlabel("Step index")
+plt.ylabel("n")
 pylab.savefig("ag_scatter.png")
