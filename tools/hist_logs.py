844aadf2461514fd386180503eb843de220571ce
Anthony Frachioni <afrachi1@binghamton.edu>
2014-12-18 12:41:35 -0800

    ignore empty histograms, turning knobs

--- tools/hist_logs.py
+++ tools/hist_logs.py
@@ -16,10 +16,13 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 
+#all_bins = np.arange(0, 15, 0.01)
+all_bins = np.arange(0, 500, 1)
 perfile = False
-column = 2
+column = 1
 dirmode = 1
 dirname = "logs/" # must end in path separator
+#dirname = "log_sub/" # must end in path separator
 single_filename = "qdotq.txt"
 
 colors = [ 'red', 'blue', 'green' ]
@@ -52,37 +55,46 @@ for filename in filenames:
 	if re.search(".lammps", filename) or re.search(".hist", filename):
 		continue
 	infile = open (dirname + filename, "r")
+
+#TODO switch based on header presence
 	line = ""
 	while not re.search("#Step", line):
 		line = infile.readline()
+	#line = infile.readline()
 
 	header = line.split()[ column ]
 	print "Working on histogram of " + header + " for " + filename
 
 	data = []
 	for line in infile:
-		data.append(float( line.split()[column] ))
+		tokens = line.split()
+		#if (int(float(tokens[0])) > 30000):
+		data.append(float( tokens[column] ))
 	infile.close()
 	del infile
 
 	weights = np.ones_like(data) / float(len(data))
 
-	n, bins, patches = plt.hist(data, bins=map(lambda x:x-0.5, np.arange(0, 15, 0.01)), facecolor=getColor(), alpha=0.75, align='mid')
-	if max(n) > max_pop:
-		max_pop = max(n)
+	if data:
+		n, bins, patches = plt.hist(data, bins=map(lambda x:x-0.5, all_bins), facecolor=getColor(), alpha=0.75, align='mid')
+		if max(n) > max_pop:
+			max_pop = max(n)
 
-	for i in range(len(n)):
-		if n[i] > 0 and bins[i] < min_Q:
-			min_Q = bins[i]
-		if n[i] > 0 and bins[i + 1] > max_Q:
-			max_Q = bins[i + 1]
+		for i in range(len(n)):
+			if n[i] > 0 and bins[i] < min_Q:
+				min_Q = bins[i]
+			if n[i] > 0 and bins[i + 1] > max_Q:
+				max_Q = bins[i + 1]
 
 	del data[:]
 
-	plt.xlabel(header)
+	#plt.xlabel(header)
+	plt.xlabel('n')
 	plt.ylabel('Frequency')
 	#plt.title(filename)
 	#plt.axis([1, 3, 0, 10000])
+	#max_pop = 3000*5/2 # XXX
+	max_pop = 100000
 	plt.axis([min_Q, max_Q, 0, max_pop])
 	plt.grid(True)
 
