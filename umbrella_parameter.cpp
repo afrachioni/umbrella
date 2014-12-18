5d072765fd86f9a6de1ebe15071e41c614b78653
Anthony Frachioni <afrachi1@binghamton.edu>
2014-12-18 12:38:09 -0800

    T = 0.741

--- umbrella_parameter.cpp
+++ umbrella_parameter.cpp
@@ -20,7 +20,8 @@ double UmbrellaParameter::compute_boltzmann_factor() {
 	current_value = param_Q->get_value();
 
 	//double temperature = *((double *) lammps_extract_compute(lmp,(char*)"thermo_temp", 0, 0));
-	double temperature = 10; //XXX
+	//double temperature = 10; //XXX
+	double temperature = 0.741; //XXX
 	//temperature = 1/8.617e-5; //XXX
 	if (temperature == 0) return -INFINITY; // Avoid nan
 
