314f1fc92a4bf573d54935cbbcf7affc8b50a987
Anthony Frachioni <frachioni@gmail.com>
2016-04-25 18:02:37 +0000

    wildcard executable name

--- examples/2particle_MC/start.bash
+++ examples/2particle_MC/start.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- mpirun -n 40 ~/driver/driver_white --windows 40 --script 2part_MC.lu --count 100000 -l
+mpirun -n 40 ~/umbrella/driver_* --windows 40 --script 2part_MC.lu --count 100000 -l
